2af69974756894fdc6098f377e8ac2607552f54d
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-10-06 16:24:59 +0300

    Complete Update

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    def __init__(self,name,breed = 'Mutt'):
+    def __init__(self,name):
         self.name = name
     pass
+
+# fido = Person()
+# fido('Kamau')
+# fido.race = 'African'
+# print(fido.__init__())
